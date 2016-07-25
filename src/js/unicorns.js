5c255c555d59d58614651253ea8006b1e02904e9
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-07-25 18:44:57 -0400

    more styling and background image.

--- src/js/unicorns.js
+++ src/js/unicorns.js
@@ -21,11 +21,11 @@ export default class Unicorns extends Component {
 	render() {
         //let message = 'Location: ' + this.state.location;
 		return (
-			<div className="content">
+			<div className="content" background-image='http://www.public-domain-image.com/free-images/wallpapers/farm-barn-scenic-landscape.jpg'>
 				<h1>Unicorns</h1>
 				<div className="unicorn-wrapper">
 					<div className="uni1">
-						<h4>George</h4>
+						<h3>George</h3>
 						<img src="http://www.scriptonitedaily.com/wp-content/uploads/2013/02/fm1.jpg" />
 							<div className="selection">
          						<select defaultValue={this.state.selectValue} onChange={::this.onChange}>
@@ -36,10 +36,10 @@ export default class Unicorns extends Component {
           					</div> 
 					</div>
 					<div className="uni2">
-						<h4>Paul</h4>
+						<h3>Paul</h3>
 						<img src="https://s-media-cache-ak0.pinimg.com/736x/8c/b0/90/8cb0901ec5847d78812d3289fc5958e6.jpg" />
 							<div className="selection">
-         						<select defaultValue={this.state.selectValue} onChange={this.onChange}>
+         						<select defaultValue={this.state.selectValue} onChange={::this.onChange}>
             					<option value="Barn">Barn</option>
             					<option value="Pasture">Pasture</option>
             					<option value="Stable">Stable</option>
@@ -47,7 +47,7 @@ export default class Unicorns extends Component {
           					</div> 
 					</div>
 					<div className="uni3">
-						<h4>John</h4>
+						<h3>John</h3>
 						<img src="https://s-media-cache-ak0.pinimg.com/564x/3d/0d/28/3d0d283c9f265cc0c930fc7398863c52.jpg" />
 							<div className="selection">
          						<select defaultValue={this.state.selectValue} onChange={this.onChange}>
@@ -58,7 +58,7 @@ export default class Unicorns extends Component {
           					</div> 
 					</div>
 					<div className="uni4">
-						<h4>Ringo</h4>
+						<h3>Ringo</h3>
 						<img src="https://s-media-cache-ak0.pinimg.com/736x/80/e0/61/80e06175e70fdb05c585a2f49d5f1db1.jpg" />
 							<div className="selection">
          						<select defaultValue={this.state.selectValue} onChange={this.onChange}>
